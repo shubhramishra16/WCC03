e78355de11a9e302547afbcacd5ad24a209e5891
shubhra <shubhramishra03@gmail.com>
2023-11-15 11:29:15 +0000

    updated project

--- git_practice/src/practice/Demo.java
+++ git_practice/src/practice/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to github");
+		System.out.println("Hello");
+		System.out.println("I am from demo");
 
 	}
 
---
+++ git_practice/src/practice/DemoDemo.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class DemoDemo {
+
+	public static void main(String[] args) {
+		System.out.println("I am from demo demo");
+	}
+
+}
